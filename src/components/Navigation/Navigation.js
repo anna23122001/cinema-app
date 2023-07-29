7703a74b60d244badabea010a9ea3b4f7beeff1a
anna <anna.riazantseva123321@gmail.com>
2023-07-29 12:41:32 +0300

    add components and db json

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navigation.css'
+
+function Navigation() {
+  return (
+    <div className='nav-container'>Navigation</div>
+  )
+}
+
+export default Navigation
---
+++ src/components/ServiceForm/ServiceForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ServiceForm.css'
+function ServiceForm() {
+  return (
+    <div>ServiceForm</div>
+  )
+}
+
+export default ServiceForm
