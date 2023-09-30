334d0c0ce1b029ce9b4f6a3fb1323b4875966627
anna <anna.riazantseva123321@gmail.com>
2023-09-30 20:33:27 +0300

    add actorController only with get and getById

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,17 @@
+
+const fs = require('fs');
+const path = require('path')
 const express = require('express');
 
 const app = express();
+
+const actorControllers = require('./controllers/actorController')
+
+// app.use(express.static('./public'));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(express.static(path.resolve('public')));
+
 
 app.get('/', (req, res) => {
     fs.readFile('./public/index.html', 'utf-8', (err, data) => {
@@ -14,6 +25,31 @@ app.get('/', (req, res) => {
     })
 });
 
+// Actors
+app.get('/actors', actorControllers.getActors)
+app.get('/actors/:actorId', actorControllers.getActorById)
+app.post('/actors', () => { })
+app.put('/actors/id', () => { })
+app.delete('/actors/id', () => { })
+// Directors
+app.get('/directors', actorControllers.)
+app.get('/directors/:directorId', actorControllers.)
+app.post('/directors', () => { })
+app.put('/directors/id', () => { })
+app.delete('/directors/id', () => { })
+// Studios
+app.get('/studios', actorControllers.)
+app.get('/studios/:studioId', actorControllers.)
+app.post('/studios', () => { })
+app.put('/studios/id', () => { })
+app.delete('/studios/id', () => { })
+// Films
+app.get('/films', actorControllers.)
+app.get('/films/:filmId', actorControllers.)
+app.post('/films', () => { })
+app.put('/films/id', () => { })
+app.delete('/films/id', () => { })
+
 app.get('/contact', (req, res) => {
     fs.readFile('./public/contact.html', 'utf-8', (err, data) => {
         if (err) {
@@ -26,15 +62,17 @@ app.get('/contact', (req, res) => {
     })
 })
 
-app.get('/images/*', (req, res) => {
-    const url = req.url;
-    fs.readFile(`./public${url}`, (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            throw err;
-        } 
-        res.setHeader('Content-Type', 'image/jpeg');
-        res.write(data);
-        res.end();
-    })
-})
+// app.get('/images/*', (req, res) => {
+//     const url = req.url;
+//     fs.readFile(`./public${url}`, (err, data) => {
+//         if (err) {
+//             res.statusCode = 404;
+//             throw err;
+//         } 
+//         res.setHeader('Content-Type', 'image/jpeg');
+//         res.write(data);
+//         res.end();
+//     })
+// })
+
+module.exports = app;
--- server/src/index.js
+++ server/src/index.js
@@ -2,99 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 
+const app = require('./app');
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    fs.readFile('./public/index.html', 'utf-8', (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            throw err;
-        }
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.write(data);
-        res.end();
-    })
-});
-
-app.get('/contact', (req, res) => {
-    fs.readFile('./public/contact.html', 'utf-8', (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            throw err;
-        } 
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.write(data);
-        res.end();
-    })
-})
-
-app.get('/images/*', (req, res) => {
-    const url = req.url;
-    fs.readFile(`./public${url}`, (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            throw err;
-        } 
-        res.setHeader('Content-Type', 'image/jpeg');
-        res.write(data);
-        res.end();
-    })
-})
 const server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`server has been started on port ${PORT}`))
 
-
-
-
-
-
-// const HOST_NAME = '127.0.0.1';
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     const method = req.method;
-//     // console.log('Headers', req.headers)
-//     console.log('Url', url)
-//     console.log('Method', method);
-
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-//     switch (url) {
-//         case '/':
-//             console.log('Home page');
-//             const homeData = fs.readFileSync('./public/index.html', 'utf8');
-//             res.write(homeData);
-//               res.end();
-//             break;
-//           case '/contact':
-//             console.log('Contact page');
-//             const contactData = fs.readFileSync('./public/contact.html', 'utf8');
-//             res.write(contactData);
-//               res.end();
-//             break;
-//         case '/actors':
-//             console.log('Actors page');
-//             res.write('<h1> Actors page </h1>');
-//               res.end();
-//             break;
-//         default:
-//             if (url.includes('/images')) {
-//                 console.log('It is image');
-//                 fs.readFile(`./public${url}`,
-//                     (err, data) => {
-//                         if (err) {
-//                             res.statusCode = 404;
-//                             throw err
-//                         }
-//                         res.write(data);
-//                         res.end();
-//                     })
-//             } else {
-//                 console.log('Page not found');
-//             res.statusCode = 404;
-//                 res.write('<h1> 404 </h1>')
-//                   res.end();
-//             }
-//         }
-// })
 
