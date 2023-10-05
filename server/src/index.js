6b43fe0d25ca7226295b156bb02ae861153be4f6
anna <anna.riazantseva123321@gmail.com>
2023-10-05 23:09:20 +0300

    state before connection db

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,33 @@ const http = require('http');
 
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+// process
+
+// Dowloading
+app.get('/download', (req, res) => {
+    console.log('download');
+    res.download(path.resolve('like.txt'))
+})
+
+// Redirect
+app.get('/phones', (req, res) => {
+    res.redirect('/contact');
+})
+
+// send query params
+
+app.get('/codes', (req, res) => {
+    console.log(first)
+    req.query
+})
+
+
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`server has been started on port ${PORT}`))
+server.listen(PORT, () =>
+    console.log(`server has been started on port ${PORT}`));
 
 
